8e0f2b699a04422b6e5303a611f54e07eddc9dc7
Amr Gamal <ag.amrgamal91@gmail.com>
2020-04-25 01:46:37 +0200

    Updates

---
+++ precache-manifest.63a15b767cb4bae412a8d1af2f2fb281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0957199c0463e62f1d01eab9c2a2c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21af90594f7dea922bb8",
+    "url": "/static/css/main.1eafb514.chunk.css"
+  },
+  {
+    "revision": "d64e4ebcfcd13e0a29a4",
+    "url": "/static/js/2.fcd62c7c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.fcd62c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21af90594f7dea922bb8",
+    "url": "/static/js/main.013bc13c.chunk.js"
+  },
+  {
+    "revision": "840a910121881ee012ae",
+    "url": "/static/js/runtime-main.d1bbc7a9.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/corona.d7265326.png"
+  }
+]);
